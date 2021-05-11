7b8f483d264d53db01de19b4926c25ddc602fa1e
NanLieu <4lieun20@solent.ac.uk>
2021-05-11 18:27:50 +0100

    updated additional ports

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,7 +14,7 @@ $("#update_user").submit(function(event){
 
 
     var request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "url" : `http://localhost:${PORT}/api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -31,7 +31,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
+            "url" : `http://localhost:${PORT}/api/users/${id}`,
             "method" : "DELETE"
         }
 
